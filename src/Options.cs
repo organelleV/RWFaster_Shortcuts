6fd173a8a8b40f233253dfdb01684bb4026900ae
organelleV <fitchw42@gmail.com>
2023-04-14 05:17:35 -0400

    files

---
+++ src/Options.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections.Generic;
+using FasterDoors;
+using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace FasterDoors
+{
+    // Based on the options script from SBCameraScroll by SchuhBaum
+    // https://github.com/SchuhBaum/SBCameraScroll/blob/Rain-World-v1.9/SourceCode/MainModOptions.cs
+    public class Options : OptionInterface
+    {
+        public static Options instance = new();
+        
+        #region Options
+
+        public static Configurable<int> TransitionSpeed = instance.config.Bind("TransitionSpeed", 110, new ConfigurableInfo(
+            "10-100, Around 2x speed. 100-500 very fast",
+            new ConfigAcceptableRange<int>(10, 500), "", "Room Transition Speed"));
+        #endregion
+
+        #region Parameters
+        private readonly float spacing = 20f;
+        private readonly float fontHeight = 20f;
+        #endregion
+
+        #region Variables
+        private Vector2 marginX = new();
+        private Vector2 pos = new();
+
+        private readonly List<float> boxEndPositions = new();
+        private readonly List<Configurable<int>> sliderConfigurables = new();
+        private readonly List<string> sliderMainTextLabels = new();
+        private readonly List<OpLabel> sliderTextLabelsLeft = new();
+        private readonly List<OpLabel> sliderTextLabelsRight = new();
+
+        private readonly List<OpLabel> textLabels = new();
+        #endregion
+
+        private const int NUMBER_OF_TABS = 1;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Tabs = new OpTab[NUMBER_OF_TABS];
+            int tabIndex = -1;
+
+            AddTab(ref tabIndex, "General");
+
+            AddSlider(TransitionSpeed, (string)TransitionSpeed.info.Tags[0], "10", "500");
+            DrawSliders(ref Tabs[tabIndex]);
+
+            AddNewLine(14);
+            DrawBox(ref Tabs[tabIndex]);
+        }
+
+        #region UI Elements
+        private void AddTab(ref int tabIndex, string tabName)
+        {
+            tabIndex++;
+            Tabs[tabIndex] = new OpTab(this, tabName);
+            InitializeMarginAndPos();
+
+            AddNewLine();
+            AddTextLabel(Plugin.MOD_NAME, bigText: true);
+            DrawTextLabels(ref Tabs[tabIndex]);
+
+            AddNewLine(0.5f);
+            AddTextLabel("Version "+Plugin.VERSION, FLabelAlignment.Left);
+            AddTextLabel("by " + Plugin.AUTHOR, FLabelAlignment.Right);
+            DrawTextLabels(ref Tabs[tabIndex]);
+
+            AddNewLine();
+            AddBox();
+        }
+
+        private void InitializeMarginAndPos()
+        {
+            marginX = new Vector2(50f, 550f);
+            pos = new Vector2(50f, 600f);
+        }
+
+        private void AddNewLine(float spacingModifier = 1f)
+        {
+            pos.x = marginX.x; // left margin
+            pos.y -= spacingModifier * spacing;
+        }
+
+        private void AddBox()
+        {
+            marginX += new Vector2(spacing, -spacing);
+            boxEndPositions.Add(pos.y); // end position > start position
+            AddNewLine();
+        }
+
+        private void DrawBox(ref OpTab tab)
+        {
+            marginX += new Vector2(-spacing, spacing);
+            AddNewLine();
+
+            float boxWidth = marginX.y - marginX.x;
+            int lastIndex = boxEndPositions.Count - 1;
+
+            tab.AddItems(new OpRect(pos, new Vector2(boxWidth, boxEndPositions[lastIndex] - pos.y)));
+            boxEndPositions.RemoveAt(lastIndex);
+        }
+
+       
+
+        private void AddSlider(Configurable<int> configurable, string text, string sliderTextLeft = "", string sliderTextRight = "")
+        {
+            sliderConfigurables.Add(configurable);
+            sliderMainTextLabels.Add(text);
+            sliderTextLabelsLeft.Add(new OpLabel(new Vector2(), new Vector2(), sliderTextLeft, alignment: FLabelAlignment.Right)); // set pos and size when drawing
+            sliderTextLabelsRight.Add(new OpLabel(new Vector2(), new Vector2(), sliderTextRight, alignment: FLabelAlignment.Left));
+        }
+
+        private void DrawSliders(ref OpTab tab)
+        {
+            if (sliderConfigurables.Count != sliderMainTextLabels.Count) return;
+            if (sliderConfigurables.Count != sliderTextLabelsLeft.Count) return;
+            if (sliderConfigurables.Count != sliderTextLabelsRight.Count) return;
+
+            float width = marginX.y - marginX.x;
+            float sliderCenter = marginX.x + 0.5f * width;
+            float sliderLabelSizeX = 0.2f * width;
+            float sliderSizeX = width - 2f * sliderLabelSizeX - spacing;
+
+            for (int sliderIndex = 0; sliderIndex < sliderConfigurables.Count; ++sliderIndex)
+            {
+                AddNewLine(2f);
+
+                OpLabel opLabel = sliderTextLabelsLeft[sliderIndex];
+                opLabel.pos = new Vector2(marginX.x, pos.y + 5f);
+                opLabel.size = new Vector2(sliderLabelSizeX, fontHeight);
+                tab.AddItems(opLabel);
+
+                Configurable<int> configurable = sliderConfigurables[sliderIndex];
+                OpSlider slider = new(configurable, new Vector2(sliderCenter - 0.5f * sliderSizeX, pos.y), (int)sliderSizeX)
+                {
+                    size = new Vector2(sliderSizeX, fontHeight),
+                    description = configurable.info?.description ?? ""
+                };
+                tab.AddItems(slider);
+
+                opLabel = sliderTextLabelsRight[sliderIndex];
+                opLabel.pos = new Vector2(sliderCenter + 0.5f * sliderSizeX + 0.5f * spacing, pos.y + 5f);
+                opLabel.size = new Vector2(sliderLabelSizeX, fontHeight);
+                tab.AddItems(opLabel);
+
+                AddTextLabel(sliderMainTextLabels[sliderIndex]);
+                DrawTextLabels(ref tab);
+
+                if (sliderIndex < sliderConfigurables.Count - 1)
+                {
+                    AddNewLine();
+                }
+            }
+
+            sliderConfigurables.Clear();
+            sliderMainTextLabels.Clear();
+            sliderTextLabelsLeft.Clear();
+            sliderTextLabelsRight.Clear();
+        }
+
+        private void AddTextLabel(string text, FLabelAlignment alignment = FLabelAlignment.Center, bool bigText = false)
+        {
+            float textHeight = (bigText ? 2f : 1f) * fontHeight;
+            if (textLabels.Count == 0)
+            {
+                pos.y -= textHeight;
+            }
+
+            OpLabel textLabel = new(new Vector2(), new Vector2(20f, textHeight), text, alignment, bigText) // minimal size.x = 20f
+            {
+                autoWrap = true
+            };
+            textLabels.Add(textLabel);
+        }
+
+        private void DrawTextLabels(ref OpTab tab)
+        {
+            if (textLabels.Count == 0)
+            {
+                return;
+            }
+
+            float width = (marginX.y - marginX.x) / textLabels.Count;
+            foreach (OpLabel textLabel in textLabels)
+            {
+                textLabel.pos = pos;
+                textLabel.size += new Vector2(width - 20f, 0.0f);
+                tab.AddItems(textLabel);
+                pos.x += width;
+            }
+
+            pos.x = marginX.x;
+            textLabels.Clear();
+        }
+        #endregion
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,129 @@
+﻿using BepInEx;
+using System;
+using System.Security.Permissions;
+using UnityEngine;
+
+
+//Credit:
+//https://github.com/Dual-Iron/catnap (MainLoopProcess_RawUpdate) method to change tick rate
+//https://github.com/Dual-Iron/TestMod.git example project used as base
+//https://github.com/SchuhBaum/SBCameraScroll/blob/Rain-World-v1.9/SourceCode/MainModOptions.cs options script
+//https://github.com/forthbridge/faster-gates/blob/main/src/Options.cs options script
+
+// figure out how to upload on steam workshop, https://github.com/forthbridge/fast-roll-button
+// create a github (catnap license), add gh links to workshop,mod json
+//clean up code, make a banner
+// make a video and screenshots
+// release!
+
+// Allows access to private members
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace FasterDoors;
+
+[BepInPlugin("com.Uten."+ MOD_ID, MOD_NAME, VERSION)]
+sealed class Plugin : BaseUnityPlugin
+{
+    public const string MOD_NAME = "Faster Shortcuts";
+    public const string MOD_ID = "FasterShortcuts";
+    public const string AUTHOR = "Uten";
+    public const string VERSION = "1.0.1";
+
+    bool init;
+
+    public void OnEnable()
+    {
+        // Add hooks here
+        On.RainWorld.OnModsInit += OnModsInit;
+        On.Player.Update += PlayerUpdateHook;
+        On.MainLoopProcess.RawUpdate += MainLoopProcess_RawUpdate;
+    }
+
+    private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        if (init) return;
+
+        init = true;
+
+        // Initialize assets, your mod config, and anything that uses RainWorld here
+        Logger.LogDebug("Faster Shortcuts");
+
+        MachineConnector.SetRegisteredOI(Plugin.MOD_ID, Options.instance);
+        Logger.LogDebug("registered: Options.instance");
+    }
+
+    bool in_door = false;
+    int last_lowerBodyFramesOnGround;
+    int last_lowerBodyFramesOffGround;
+
+    void PlayerUpdateHook(On.Player.orig_Update orig, Player self, bool eu)
+    {
+        /* This method will be subscribed to Player.Update. */
+        orig(self, eu);
+        /*Logger.LogDebug("PlayerUpdateHook:animationvalue: " + self.animation.value);
+        Logger.LogDebug("PlayerUpdateHook:animationframe: " + self.animationFrame);
+        Logger.LogDebug("PlayerUpdateHook:standing: " + self.standing);
+        Logger.LogDebug("PlayerUpdateHook:onground: " + self.lowerBodyFramesOnGround);
+        Logger.LogDebug("PlayerUpdateHook:offground: " + self.lowerBodyFramesOffGround);*/
+        
+        // if on and off ground counters are not being updated, slugcat
+        // is room transitioning or dead or in another state
+        if (last_lowerBodyFramesOnGround == self.lowerBodyFramesOnGround 
+        && last_lowerBodyFramesOffGround == self.lowerBodyFramesOffGround
+        && !self.dead)
+        {
+            in_door = true;
+        }
+        else
+        {
+            in_door = false;
+        }
+
+        last_lowerBodyFramesOnGround = self.lowerBodyFramesOnGround;
+        last_lowerBodyFramesOffGround = self.lowerBodyFramesOffGround;
+    }
+
+    private void MainLoopProcess_RawUpdate(On.MainLoopProcess.orig_RawUpdate orig, MainLoopProcess self, float dt)
+    {
+        try
+        {
+            orig(self, dt);
+
+            if (self is not RainWorldGame game || !game.IsStorySession || game.pauseMenu != null || !game.processActive)
+            {
+                return;
+            }
+
+
+            // If it takes more than one "tick duration" to finish an update, the game is lagging. Normally, the game would tick multiple times to make up for this,
+            // but we don't want to make the lag worse, so we simply pretend only one "tick duration" has passed.
+            if (dt > 1 / 40f)
+                dt = 1 / 40f;
+
+            // run game up to N× faster while room transitioning
+            var gameSpeed = Options.TransitionSpeed.Value;
+
+            if (in_door == true)
+            {
+                self.myTimeStacker += dt * gameSpeed;
+                while (self.myTimeStacker > 1)
+                {
+                    self.myTimeStacker -= 1;
+                    //self.manager.rainWorld.rewiredInputManager.SendMessage("Update");
+                    self.Update();
+                }
+                //self.GrafUpdate(self.myTimeStacker);
+            }
+        }
+        catch (Exception e)
+        {
+            Logger.LogError($"Main loop process {self.GetType()} threw an uncaught exception.\n" + e);
+        }
+    }
+
+
+}
